37bd500e2aaf95ebdcd6cfd9031f02e887b2a210
samubernard <samubernard@gmail.com>
2020-03-19 09:35:50 +0100

    French confirmed, deaths, recovered datasets

---
+++ src/datasets/parse_datasets.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ---------------
+# Confirmed cases
+# extract headers
+head -2 time_series-ncov-Confirmed.csv  >confirmed_cases_metr_france.csv
+
+# extract lines from Metropolitan France (France,France)
+sed -n '/^\s*France\s*,\s*France/p' time_series-ncov-Confirmed.csv >>confirmed_cases_metr_france.csv
+
+# ---------------
+# Death cases  
+# extract headers
+head -2 time_series-ncov-Deaths.csv  >death_cases_metr_france.csv
+
+# extract lines from Metropolitan France (France,France)
+sed -n '/^\s*France\s*,\s*France/p' time_series-ncov-Deaths.csv >>death_cases_metr_france.csv
+
+# ---------------
+# Death cases  
+# extract headers
+head -2 time_series-ncov-Recovered.csv  >recovered_cases_metr_france.csv
+
+# extract lines from Metropolitan France (France,France)
+sed -n '/^\s*France\s*,\s*France/p' time_series-ncov-Recovered.csv >>recovered_cases_metr_france.csv
+
