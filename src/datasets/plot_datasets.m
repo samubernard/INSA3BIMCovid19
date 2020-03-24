7b589eff509677c8e7c7110f76dbd15a06129b22
samubernard <samubernard@gmail.com>
2020-03-24 18:12:36 +0100

    updated plot styles

--- src/datasets/plot_datasets.m
+++ src/datasets/plot_datasets.m
@@ -7,18 +7,18 @@ recovered = readtable('recovered_cases_metr_france.csv', 'ReadVariableNames', tr
 %% Plot datasets on a single axes
 
 figure(1); clf;
-semilogy(confirmed.Date, confirmed.Value)
+semilogy(confirmed.Date, confirmed.Value,'LineWidth',2)
 hold on % do not replace data on plot
-semilogy(death.Date, death.Value)
-semilogy(recovered.Date, recovered.Value)
+semilogy(death.Date, death.Value,'LineWidth',2)
+semilogy(recovered.Date, recovered.Value,'LineWidth',2)
 
 % text(datetime('2020-01-29'),1e3,'https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases')
 
 % add gov measures
-line(datetime({'2020-03-16','2020-03-16'}),[1e0,1e4],'Color','r', 'LineStyle', '-.');
-line(datetime({'2020-03-17','2020-03-17'}),[1e0,1e4],'Color','b', 'LineStyle', '--');
+line(datetime({'2020-03-16','2020-03-16'}),[1e0,1e5],'Color','r', 'LineStyle', '-.');
+line(datetime({'2020-03-17','2020-03-17'}),[1e0,1e5],'Color','b', 'LineStyle', '--');
 
-legend('Confirmed', 'Deaths', 'Recovered','Schools closed','Containement','Location','northwest')
+legend('Confirmed', 'Deaths', 'Recovered','Schools closed','Isolation','Location','northwest')
 xlabel('Date')
 ylabel('Number')
 title({'Covid-19 in France';'source: https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases'})
